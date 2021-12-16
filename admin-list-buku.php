cecea16cc9c5894d84fc0beec1c55b8f4a1c9049
Nicola Alivant <nicola.alivant@gmail.com>
2021-12-16 21:11:15 +0700

    Add bayar denda sidebar item

--- admin-list-buku.php
+++ admin-list-buku.php
@@ -115,6 +115,7 @@ $data_buku = query("SELECT * FROM tblbuku");
                         <li> <a class="waves-effect waves-dark" href="admin-peminjaman.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Peminjaman</span></a></li>
                         <li> <a class="waves-effect waves-dark" href="admin-kembali.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Pengembalian</span></a></li>
                         </li>
+                    <li> <a class="waves-effect waves-dark" href="admin-denda.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Bayar Denda</span></a></li></li>
                         <li> <a class="waves-effect waves-dark" href="user.php" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">User</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="admin.php" aria-expanded="false"><i class="mdi mdi-account-check"></i><span class="hide-menu">Admin</span></a>
