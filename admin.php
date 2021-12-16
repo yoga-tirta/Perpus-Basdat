ba3026f5b11f0b8246fb7521bc9e0278dfda49e8
Nicola Alivant <nicola.alivant@gmail.com>
2021-12-16 21:12:31 +0700

    Add bayar denda sidebar item

--- admin.php
+++ admin.php
@@ -125,6 +125,7 @@ $adminName = $_SESSION['namaadmin'];
                         </li>
                         <li> <a class="waves-effect waves-dark" href="admin-peminjaman.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Peminjaman</span></a></li>
                         <li> <a class="waves-effect waves-dark" href="admin-kembali.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Pengembalian</span></a></li>
+                      <li> <a class="waves-effect waves-dark" href="admin-denda.php" aria-expanded="false"><i class="mdi mdi-table"></i><span class="hide-menu">Bayar Denda</span></a></li></li>
                         <li> <a class="waves-effect waves-dark" href="user.php" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">User</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="admin.php" aria-expanded="false"><i class="mdi mdi-account-check"></i><span class="hide-menu">Admin</span></a>
